735181a9d230e92662e26f6370d590b6aa5bac9d
Szabo Gergely <gege251@mailbox.org>
2025-03-06 15:10:09 +0100

    Update flake

--- flake.nix
+++ flake.nix
@@ -7,29 +7,37 @@
     cardano-node.url = "github:IntersectMBO/cardano-node/10.1.4";
   };
 
-  outputs = inputs@{ flake-parts, ... }: {
-    flakeModule = ./cardano-devnet.nix;
-  } //
-  flake-parts.lib.mkFlake { inherit inputs; } {
-    systems = [ "x86_64-linux" "aarch64-linux" "aarch64-darwin" "x86_64-darwin" ];
+  outputs =
+    inputs@{ flake-parts, ... }:
+    {
+      flakeModule = ./cardano-devnet.nix;
+    }
+    // flake-parts.lib.mkFlake { inherit inputs; } {
+      systems = [
+        "x86_64-linux"
+        "aarch64-linux"
+        "aarch64-darwin"
+        "x86_64-darwin"
+      ];
 
-    imports = [ ./cardano-devnet.nix ];
+      imports = [ ./cardano-devnet.nix ];
 
-    debug = true;
+      debug = true;
 
-    perSystem = { pkgs, config, ... }: {
-      cardano-devnet.initialFunds = {
-        "609783be7d3c54f11377966dfabc9284cd6c32fca1cd42ef0a4f1cc45b" = 900000000000;
-      };
-
-      devShells.default = pkgs.mkShell
+      perSystem =
+        { pkgs, config, ... }:
         {
+          cardano-devnet.initialFunds = {
+            "609783be7d3c54f11377966dfabc9284cd6c32fca1cd42ef0a4f1cc45b" = 900000000000;
+          };
 
-          buildInputs = [
-            config.packages.cardano-devnet
-          ];
+          devShells.default = pkgs.mkShell {
+
+            buildInputs = [
+              config.packages.cardano-devnet
+            ];
+          };
+
         };
-
     };
-  };
 }
