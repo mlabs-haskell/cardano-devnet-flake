46d33c3aa96ea5b8998a62539974fa80f22dd6d8
Szabo Gergely <gege251@mailbox.org>
2025-06-10 17:05:07 +0200

    Move options to a separate file

--- process-compose-module.nix
+++ process-compose-module.nix
@@ -56,37 +56,7 @@ let
   mod =
     { config, name, ... }:
     {
-      options = {
-        cardano-node = lib.mkPackageOption pkgs "cardano-node" { };
-
-        cardano-cli = lib.mkPackageOption pkgs "cardano-cli" { };
-
-        initialFundsKeyType = lib.mkOption {
-          type = lib.types.string;
-          example = "bech32-binary";
-          default = "verification-key-hash";
-          description = "Flag to define the keys used in `initialFunds` option (bech32-binary | verification-key-file | verification-key-hash)";
-        };
-
-        initialFunds = lib.mkOption {
-          type = lib.types.attrsOf lib.types.ints.unsigned;
-          example = {
-            "9783be7d3c54f11377966dfabc9284cd6c32fca1cd42ef0a4f1cc45b" = 900000000000;
-          };
-          description = "Wallet public key - lovelace pair, determining the initial funds. Use `initialFundsKeyType` define the key type you're using.";
-        };
-        networkMagic = lib.mkOption {
-          type = lib.types.ints.unsigned;
-          default = 42;
-          example = 42;
-          description = "Cardano network magic id";
-        };
-        dataDir = lib.mkOption {
-          type = lib.types.str;
-          default = "./data/cardano-devnet";
-          description = "The directory where all data for `cardano-devnet.<name>` is stored";
-        };
-      };
+      options = import ./options.nix { inherit lib pkgs; };
 
       config.outputs.settings.processes."${name}" =
         let
